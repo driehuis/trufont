131e71f5309ecd7dd09442f7585070c63aa53c4e
Bert Driehuis <driehuis@playbeing.org>
2021-10-26 00:24:10 +0200

    Next try overriding 0.0.0+unknown

--- Lib/trufont/controls/aboutDialog.py
+++ Lib/trufont/controls/aboutDialog.py
@@ -41,7 +41,7 @@ PATH = os.path.abspath(os.path.join(modulePath, "../../.."))
 if 'Briefcase-Version' in metadata:
     PATH = os.path.join(PATH, "app")
     gitShortHash = "briefcase"
-    if __version__ == '0.0.0':
+    if __version__ == '0.0.0+unknown':
         __version__ = metadata['Version']
     try:
         with open(os.path.join(PATH, "AUTHORS"), encoding="utf-8") as fd:
